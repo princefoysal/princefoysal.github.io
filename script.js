10007ab0c7e95f32f8e54d81588d54427e1393b8
princefoysal <51753143+princefoysal@users.noreply.github.com>
2019-09-14 12:40:20 +0600

    doop
    doop

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-$(window).bind('scroll', function() {
-    var currentTop = $(window).scrollTop();
-    var elems = $('section');
-    elems.each(function(index){
-      var elemTop 	= $(this).offset().top;
-      var elemBottom 	= elemTop + $(this).height();
-      if(currentTop >= elemTop && currentTop <= elemBottom){
-        var id 		= $(this).attr('id');
-        var navElem = $('a[href="#' + id+ '"]');
-    navElem.parent().addClass('active').siblings().removeClass( 'active' );
-      }
-    })
+$(".slider").slick({
+	infinite:'true',
+	slidesToShow:1,
+	slidesToScroll:1
 });
+	$('#textbanner').textbanner();
