cfcef7ad0e3a0fb78445657b5def7b1efbd3d36e
princefoysal <51753143+princefoysal@users.noreply.github.com>
2019-09-14 12:20:47 +0600

    mizie
    mizie

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$(".slider").slick({
-	infinite:'true',
-	slidesToShow:1,
-	slidesToScroll:1
+$(window).bind('scroll', function() {
+    var currentTop = $(window).scrollTop();
+    var elems = $('section');
+    elems.each(function(index){
+      var elemTop 	= $(this).offset().top;
+      var elemBottom 	= elemTop + $(this).height();
+      if(currentTop >= elemTop && currentTop <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
 });
-	$('#textbanner').textbanner();
